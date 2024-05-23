686f0dc1a1024f07a85e9d485c293352e9603130
Miel Vander Sande <miel.vandersande@meemoo.be>
2024-05-23 12:23:35 +0200

    Add since param to sync

--- flows/main_flow.py
+++ flows/main_flow.py
@@ -1,12 +1,13 @@
-from prefect import task, flow, logger
+from prefect import task, flow, get_run_logger
 import requests
 import psycopg2
 from io import StringIO
 from prefect_meemoo.triplydb.credentials import TriplyDBCredentials
 from prefect_sqlalchemy.credentials import DatabaseCredentials
+import datetime
 
 @task()
-def upsert_page(table_name, csv_url, postgres_creds, triply_creds):
+def upsert_page(table_name, csv_url, postgres_creds, triply_creds, since = None):
     # Load logger
     logger = get_run_logger()
 
@@ -22,7 +23,8 @@ def upsert_page(table_name, csv_url, postgres_creds, triply_creds):
 
     def fetch_and_insert_page(url, cursor, temp_table_name):
         """Fetch a single page of CSV data and insert it into the temporary table."""
-        response = requests.get(url, headers={
+        params = {"since": since.isoformat()} if since is not None else {}
+        response = requests.get(url, params=params, headers={
             "Authorization": "Bearer " + triply_creds.token,
             "Accept": "text/csv"
         })
@@ -74,7 +76,7 @@ def upsert_page(table_name, csv_url, postgres_creds, triply_creds):
 def main_flow(
     triplydb_block_name: str = "triplydb",
     postgres_block_name: str = "hasura",
-    full_sync: bool = False,
+    since: datetime = None,
     ):
     """
     Here you write your main flow code and call your tasks and/or subflows.
@@ -94,7 +96,7 @@ def main_flow(
 
     # For each entry in config table: start sync task
     for table_name, csv_url in table_config.items():
-        upsert_page(table_name=table_name, csv_url=csv_url, triply_creds=triply_creds, postgres_creds=postgres_creds).submit()
+        upsert_page(table_name=table_name, csv_url=csv_url, triply_creds=triply_creds, postgres_creds=postgres_creds, since=since).submit()
 
 
 
