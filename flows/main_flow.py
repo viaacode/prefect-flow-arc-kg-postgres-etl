89d678a063cd5f63bfe15a4521ed3e63fb773fc3
Miel Vander Sande <miel.vandersande@meemoo.be>
2024-06-05 10:58:36 +0200

    Rename files back to table name and adjust register script. Add logging to main flow

--- flows/main_flow.py
+++ flows/main_flow.py
@@ -4,30 +4,37 @@ import psycopg2
 from io import StringIO
 from prefect_meemoo.triplydb.credentials import TriplyDBCredentials
 from prefect_sqlalchemy.credentials import DatabaseCredentials
+from prefect.blocks.system import JSON
 import datetime
 
+
 @task()
-def upsert_pages(table_name, csv_url, postgres_creds, triply_creds, since = None):
+def upsert_pages(table_name, csv_url, postgres_creds, triply_creds, since=None):
     # Load logger
     logger = get_run_logger()
 
     def get_next_page_link(response):
         """Extract the next page link from the Link header if it exists."""
-        link_header = response.headers.get('Link', None)
+        link_header = response.headers.get("Link", None)
         if link_header:
-            links = requests.utils.parse_header_links(link_header.strip('<>'))
+            links = requests.utils.parse_header_links(link_header.strip("<>"))
             for link in links:
-                if link.get('rel') == 'next':
-                    return link['url']
+                if link.get("rel") == "next":
+                    return link["url"]
         return None
 
     def fetch_and_insert_page(url, cursor, temp_table_name):
         """Fetch a single page of CSV data and insert it into the temporary table."""
         params = {"since": since.isoformat()} if since is not None else {}
-        response = requests.get(url, params=params, headers={
-            "Authorization": "Bearer " + triply_creds.token,
-            "Accept": "text/csv"
-        })
+        logger.info(f"Fetch and insert page {url} into {table_name}")
+        response = requests.get(
+            url,
+            params=params,
+            headers={
+                "Authorization": "Bearer " + triply_creds.token,
+                "Accept": "text/csv",
+            },
+        )
         response.raise_for_status()
 
         csv_data = StringIO(response.text)
@@ -41,7 +48,7 @@ def upsert_pages(table_name, csv_url, postgres_creds, triply_creds, since = None
     cur = conn.cursor()
 
     # Step 2: Create a temporary table for upserting
-    temp_table_name = f'temp_{table_name}'
+    temp_table_name = f"temp_{table_name}"
     create_temp_table_query = f"""
     CREATE TEMP TABLE {temp_table_name} (LIKE {table_name} INCLUDING ALL);
     """
@@ -56,6 +63,7 @@ def upsert_pages(table_name, csv_url, postgres_creds, triply_creds, since = None
         url = get_next_page_link(requests.get(url))
 
     # Step 4: Upsert from the temporary table to the actual table
+    logger.info(f"Upsert from the temporary table {temp_table_name} to the actual table {table_name}")
     upsert_query = f"""
     INSERT INTO {table_name}
     SELECT * FROM {temp_table_name}
@@ -70,25 +78,22 @@ def upsert_pages(table_name, csv_url, postgres_creds, triply_creds, since = None
     cur.close()
     conn.close()
 
-    return logger.info("Data inserted successfully.")
+    return logger.info(f"Table {table_name} successfully synced.")
+
 
 @flow(name="FILL_IN_FLOW_NAME_HERE!")
 def main_flow(
     triplydb_block_name: str = "triplydb",
     postgres_block_name: str = "hasura",
+    config_block_name: str = "saved-query-config",
     since: datetime = None,
-    ):
+):
     """
     Here you write your main flow code and call your tasks and/or subflows.
     """
 
-    # Load logger
-    logger = get_run_logger()
-    
-    # Configuration
-    table_config = {
-        'graph.intellectual_entity': 'https://api.meemoo.triply.cc/queries/meemoo/Query/11/run'
-    }
+    # Load configuration
+    table_config = JSON.load(config_block_name)
 
     # Load credentials
     triply_creds = TriplyDBCredentials.load(triplydb_block_name)
@@ -96,8 +101,10 @@ def main_flow(
 
     # For each entry in config table: start sync task
     for table_name, csv_url in table_config.items():
-        upsert_pages(table_name=table_name, csv_url=csv_url, triply_creds=triply_creds, postgres_creds=postgres_creds, since=since).submit()
-
-
-
-
+        upsert_pages(
+            table_name=table_name,
+            csv_url=csv_url,
+            triply_creds=triply_creds,
+            postgres_creds=postgres_creds,
+            since=since,
+        ).submit()
--- queries/register.js
+++ queries/register.js
@@ -2,6 +2,7 @@
 import App from '@triply/triplydb'
 
 import 'dotenv/config';
+import { writeFileSync } from 'fs'
 import { readdir, readFile } from 'fs/promises'
 import { join, extname, parse } from 'path'
 
@@ -12,6 +13,7 @@ const triply = App.get({ token: process.env.TOKEN })
 async function run() {
   const account = await triply.getAccount(process.env.ACCOUNT)
   const dataset = await account.getDataset(process.env.DATASET)
+  const result = {}
 
   try {
     const files = await readdir(directoryPath)
@@ -23,16 +25,31 @@ async function run() {
       if (extname(file) === '.sparql') {
         try {
           const queryString = await readFile(filePath, 'utf8')
-          const queryName = `get-${parse(filePath).name}`
-          console.log(`Adding contents of ${file} as ${queryName}\n`)
+          const tableName = parse(filePath).name
+          const queryName = `get-${tableName.replace(/[._]/g, '-')}`
           
-          const query = await account.addQuery(queryName, {
+          console.log(`Registering contents of ${file} as Saved Query '${queryName}'\n`)
+          
+          let query;
+          const params = {
             dataset,
             queryString,
             serviceType: 'virtuoso',
             output: 'response',
-          })
-          console.log(`Available on ${await query.getRunLink()}\n`)
+          }
+
+          try {
+            query = await account.getQuery(queryName)
+            await query.delete()
+            console.log(`Query ${queryName} deleted.\n`)
+          } catch (error) {
+            console.log(`Query ${queryName} does not exist.\n`)
+          }
+          query = await account.addQuery(queryName, params)
+          const runLink = await query.getRunLink()
+
+          result[tableName] = runLink
+          console.log(`Available on ${runLink}\n`)
           console.log('\n--------------------------------------------\n')
         } catch (readErr) {
           console.error(readErr)
@@ -42,7 +59,7 @@ async function run() {
   } catch (dirErr) {
     console.error('Unable to scan directory: ' + dirErr)
   }
-
+  writeFileSync('config.json', JSON.stringify(result))
 }
 run(directoryPath).catch(e => {
   console.error(e)
